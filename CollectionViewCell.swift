97aa5dd76983e7674e91c9de1f7d67f78d3067d9
keewonma <jonathankma@gmail.com>
2016-04-26 15:17:29 +0900

    ImageView in CollectionView
    IBoutlet for image view in collection view

---
+++ CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  
+//
+//  Created by Jonathan Ma on 4/26/16.
+//
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
